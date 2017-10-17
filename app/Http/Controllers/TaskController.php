bbbc5f7ba1702c7749470bf114beeff7749a0469
s3a317097 <s3a317097@users.noreply.github.com>
2017-10-17 17:00:47 +0800

    注入資源庫

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,38 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Task;
-use App\User;
 use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
+    /**
+     * 任務資源庫的實例。
+     *
+     * @var TaskRepository
+     */
     protected $tasks;
-    public function __construct()
+
+    /**
+     * 建立新的控制器實例。
+     *
+     * @param  TaskRepository  $tasks
+     * @return void
+     */
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
-    }
-    public function index(Request $request){
-        $tasks = Task::where('user_id', $request->user()->id)->get();
 
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
-
-        //return view('tasks.index');
+        $this->tasks = $tasks;
     }
-    public function store(Request $request)
+
+    /**
+     * 取得給定使用者的所有任務。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
     {
-        
-        $this->validate($request, [
-            'name' => 'required|max:255',
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
         ]);
-
-        $request->User()->tasks()->create([
-            'name' => $request->name,
-        ]);
-
-        return redirect('/tasks');
     }
 }
