f4afcc00140e084f81f8a1657d39600a033e0216
s3a317097 <s3a317097@users.noreply.github.com>
2017-10-17 16:12:11 +0800

    增加任務1

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,11 +8,20 @@ use App\Http\Requests;
 
 class TaskController extends Controller
 {
-    public function __construct()
+    /*public function __construct()
     {
         $this->middleware('auth');
-    }
+    }*/
     public function index(Request $request){
-        return view('task.index');
+       
+        return view('tasks.index');
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        // Create The Task...
     }
 }
