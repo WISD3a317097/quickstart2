cd2b8331d722711fa7ca255e63e87fcbd3e20314
s3a317097 <s3a317097@users.noreply.github.com>
2017-10-17 16:24:35 +0800

    顯示已有的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,25 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Task;
 use App\Http\Requests;
 
 class TaskController extends Controller
 {
-    /*public function __construct()
+    protected $tasks;
+    public function __construct()
     {
         $this->middleware('auth');
-    }*/
+    }
     public function index(Request $request){
        
-        return view('tasks.index');
+       $tasks = Task::where('user_id', $request->user()->id)->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
     }
     public function store(Request $request)
     {
+        
         $this->validate($request, [
             'name' => 'required|max:255',
         ]);
 
-        // Create The Task...
+        $request->User()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
     }
 }
