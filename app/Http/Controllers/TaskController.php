bab4007a72b0d9173f4818fdbb7fc80b7dcc256c
s3a317097 <s3a317097@users.noreply.github.com>
2017-10-17 16:48:42 +0800

    認證重作

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Task;
+use App\User;
 use App\Http\Requests;
 
 class TaskController extends Controller
@@ -14,12 +15,13 @@ class TaskController extends Controller
         $this->middleware('auth');
     }
     public function index(Request $request){
-       
-       $tasks = Task::where('user_id', $request->user()->id)->get();
+        /*$tasks = Task::where('user_id', $request->user()->id)->get();
 
         return view('tasks.index', [
             'tasks' => $tasks,
-        ]);
+        ]);*/
+
+        return view('tasks.index');
     }
     public function store(Request $request)
     {
